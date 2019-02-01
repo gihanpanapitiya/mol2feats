b037db49a50e5c393f2daa42c4482dfb5f52c08a
Gihan Uthpala Panapitiya <gihanuthpala@gmail.com>
2019-01-31 20:03:57 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+    name="mol2feats",
+    version="0.1.0",
+    url="https://github.com/username/animalsounds",
+    author="Gihan Panapitiya",
+    author_email="gihanuthpala@gmail.com",
+    description="A collection of tools to generate features from a molecule",
+    long_description=open('README.rst').read(),
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
