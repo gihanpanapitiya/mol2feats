e825d5a62d898f1c2110b8ed51bf23bbfb26dc52
Gihan Uthpala Panapitiya <gihanuthpala@gmail.com>
2019-01-31 20:08:59 -0500

    Create dist.py

---
+++ dist.py
@@ -0,0 +1,24 @@
+import utils
+import numpy as np
+
+
+utils = utils.utils()
+def get_cluster_from_rs(pos, site):
+    # f2 = utils()
+
+    pos = pos
+    site = site
+
+
+    if len(pos) == 0:
+        cluster_f, centroid = 0,0
+
+    elif len(pos) == 1:
+        centroid = np.mean(pos, axis=0)
+        cluster_f  = utils.dist_between_two(pos , centroid)
+
+    else:
+        centroid = np.mean(pos, axis=0)
+        cluster_f  = np.mean(np.sqrt(np.sum((pos - centroid)**2, axis=1)))
+
+    return (cluster_f, centroid)
